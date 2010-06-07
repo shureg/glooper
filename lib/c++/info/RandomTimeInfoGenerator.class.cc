c606441ac7c01c1b8e494676aa9fc774e2fd7eb0
Alexander Kabanov <alexander.kabanov@gmail.com>
2010-06-07 15:55:04 +0100

    Added the RandomTimeInfogenerator class

---
+++ lib/c++/info/RandomTimeInfoGenerator.class.cc
@@ -0,0 +1,31 @@
+// =====================================================================================
+// 
+//       Filename:  RandomTimeInfoGenerator.class.cc
+// 
+//    Description:  
+// 
+//        Version:  1.0
+//        Created:  07/06/10 13:30:34
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Alexander Kabanov (aak), alexander.kabanov@gmail.com
+//        Company:  
+// 
+// =====================================================================================
+
+#include "info/RandomTimeInfoGenerator.class.h"
+
+using GLOOPER_TEST::RandomTimeInfoGenerator;
+
+RandomTimeInfoGenerator::RandomTimeInfoGenerator(
+      const bspTRGd& value_generator,
+      const bspTRGd& event_generator):
+   InfoGenerator(value_generator),
+   event_generator(event_generator)
+{}
+
+const bool RandomTimeInfoGenerator::generate_now(unsigned long turn_timer)
+{
+   return ( U() <= (*event_generator)() );
+}
